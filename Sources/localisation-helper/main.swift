e74252d665834c01b3767c10698402f3eca72a96
Ксения <56629590+ksko-git@users.noreply.github.com>
2020-11-07 21:25:17 +0700

    Added new variables to the "Container" class

--- Sources/localisation-helper/main.swift
+++ Sources/localisation-helper/main.swift
@@ -5,6 +5,15 @@ class Container {
     var argumentsParser: ArgumentsParserProtocol {
         return ArgumentsParser()
     }
+    var search: SearchProtocol {
+        return Search()
+    }
+    var update: UpdateProtocol {
+        return Update()
+    }
+    var delete: DeleteProtocol {
+        return Delete()
+    }
 }
 
 func main() {
@@ -13,40 +22,35 @@ func main() {
 
     let arguments = container.argumentsParser.parse()
     
+    
     if case .search(let key, let language) = arguments {
-        let search = Search()
-        
         if let key: String = key, let language: String = language {
-            search.search(key: key, language: language)
+            container.search.search(key: key, language: language)
         } else if let key: String = key {
-            search.search(key: key, language: "")
+            container.search.search(key: key, language: "")
         } else if let language: String = language {
-            search.search(key: "", language: language)
+            container.search.search(key: "", language: language)
         } else {
-            search.defaultSearch()
+            container.search.defaultSearch()
         }
         
     } else if case .update(let word, let key, let language) = arguments {
-        let update = Update()
-        
         if let thisWord = word, let key: String = key, let language: String = language {
-            update.update(newWord: thisWord, key: key, language: language)
+            container.update.update(newWord: thisWord, key: key, language: language)
         } else if let thisWord = word, let key: String = key {
-            update.update(newWord: thisWord, key: key, language: "")
+            container.update.update(newWord: thisWord, key: key, language: "")
         } else if let thisWord = word, let language: String = language {
-            update.update(newWord: thisWord, key: "", language: language)
+            container.update.update(newWord: thisWord, key: "", language: language)
         } else {
             print(Commands.helpMessage())
         }
-    } else if case .delete(let key, let language) = arguments {
-        let delete = Delete()
-        
+    } else if case .delete(let key, let language) = arguments {        
         if let key: String = key, let language: String = language {
-            delete.delete(key: key, language: language)
+            container.delete.delete(key: key, language: language)
         } else if let key: String = key {
-            delete.delete(key: key, language: "")
+            container.delete.delete(key: key, language: "")
         } else if let language: String = language {
-            delete.delete(key: "", language: language)
+            container.delete.delete(key: "", language: language)
         } else {
             print(Commands.helpMessage())
         }
