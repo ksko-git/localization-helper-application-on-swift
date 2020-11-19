9f5e0fa7bb6e37b9b16d5572fd2b689c7b362cac
Ксения <56629590+ksko-git@users.noreply.github.com>
2020-11-20 01:10:12 +0700

    Tests added in allTests

--- Tests/localisation-helperTests/XCTestManifests.swift
+++ Tests/localisation-helperTests/XCTestManifests.swift
@@ -5,8 +5,9 @@ public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(localisation_helperTests.allTests),
         testCase(TestArgumentParser.allTests),
-        testCase(TestSearchOption.allTests)
-        
+        testCase(TestSearchOption.allTests),
+        testCase(TestDeleteOption.allTests),
+        testCase(TestUpdateOption.allTests)        
     ]
 }
 #endif
